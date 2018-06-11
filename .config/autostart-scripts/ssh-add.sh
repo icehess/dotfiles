a648f4beac48aadc124c40d87da9688e041f8898
Hesaam Farhang <hesaam@2600hz.com>
2018-06-10 23:30:05 -0700

    fix ssh_agent and kwallet

--- .config/autostart-scripts/ssh-add.sh
+++ .config/autostart-scripts/ssh-add.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-[ type ssh-add > /dev/null 2>&1 ] && [ -f $HOME/.ssh/hesaam_github ] && ssh-add $HOME/.ssh/hesaam_github </dev/null
+
+if type ssh-add > /dev/null 2>&1 ; then
+    if [ -f $HOME/.ssh/hesaam_github ] ; then
+        ssh-add $HOME/.ssh/hesaam_github </dev/null
+    else
+        echo "no github keys were found."
+        exit 1
+    fi
+else
+    echo "ssh-add command not found"
+    exit 1
+fi
