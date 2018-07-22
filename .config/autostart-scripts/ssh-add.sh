008678207eb4288401bde28102dbedaa86b820cf
Hesaam Farhang <icehess@gmail.com>
2018-07-22 15:25:25 -0700

    fix ssh load and home display

--- .config/autostart-scripts/ssh-add.sh
+++ .config/autostart-scripts/ssh-add.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 if type ssh-add > /dev/null 2>&1 ; then
-    if [ -f $HOME/.ssh/2600hz_hesaam ] ; then
-        ssh-add $HOME/.ssh/2600hz_hesaam </dev/null
-    else
-        echo "no 2600Hz keys were found."
-        exit 1
-    fi
+    FILES="$HOME/.ssh/2600hz_hesaam $HOME/.ssh/hesaam_github"
+    for file in $FILES ; do
+        if [ -f $file ] ; then
+            ssh-add $HOME/.ssh/$file </dev/null
+        fi
+    done
 else
     echo "ssh-add command not found"
     exit 1
