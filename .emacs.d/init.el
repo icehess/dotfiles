b0c84ff7ef827a3ff1e840bc4e56917c4700946b
Hesaam Farhang <icehess@gmail.com>
2019-03-05 20:49:08 -0800

    set color theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4d827eb0ab3de50c6472e576bfc4fca5a3e5466bdc5e4944e2d6128032f15e03" default)))
+ '(package-selected-packages
+   (quote
+    (erlang try which-key use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil)))))
+
+
+;; my stuff
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
@@ -43,27 +64,4 @@
   (require 'erlang-start))
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-
-(use-package doom-themes
-  :ensure t)
-
-(use-package sublime-themes
-  :ensure t)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("4d827eb0ab3de50c6472e576bfc4fca5a3e5466bdc5e4944e2d6128032f15e03" "01595d2ae74b67ef7c93173a9ff8eed1c7ac0a167d35fd095e06a04d4f577be1" "bca0930505c4fc2597c1a65b1612c66e4d30b2d996756cb278cc0981087063d6" "d3711e8381ea8f13f8d32db760990221f16304fa207f507af47ceb5c299713e2" "39dd7106e6387e0c45dfce8ed44351078f6acd29a345d8b22e7b8e54ac25bac4" "cc0dbb53a10215b696d391a90de635ba1699072745bf653b53774706999208e3" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "0d456bc74e0ffa4bf5b69b0b54dac5104512c324199e96fc9f3a1db10dfa31f3" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "2d15316ddb0b94c9c8b4c6cc77ef68486ff738d05bbb6edefec0dca7eafedcae" "ab564a7ce7f2b2ad9e2cfe9fe1019b5481809dd7a0e36240c9139e230cc2bc32" "f11e219c9d043cbd5f4b2e01713c2c24a948a98bed48828dc670bd64ae771aa1" "3bc187cd480ad79f151b593f7cb7d4ad869b19741247589238c353f637e7fb21" "fa2af0c40576f3bde32290d7f4e7aa865eb6bf7ebe31eb9e37c32aa6f4ae8d10" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "c9b89349d269af4ac5d832759df2f142ae50b0bbbabcce9c0dd53f79008443c9" "e7b49145d311e86da34a32a7e1f73497fa365110a813d2ecd8105eaa551969da" "d057f0430ba54f813a5d60c1d18f28cf97d271fd35a36be478e20924ea9451bd" default)))
- '(package-selected-packages
-   (quote
-    (doom-themes sublime-themes erlang try which-key use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background nil)))))
+(load-theme 'iceberg)
