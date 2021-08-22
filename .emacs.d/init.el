b6bc293d5fd15b9af0765e6b8ed577bd8da9db30
Hesaam Farhang <icehess@gmail.com>
2021-08-21 23:44:12 -0700

    emacs lsp?

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -198,20 +198,110 @@
 
 
 
+(use-package flycheck
+    :ensure t
+    :init)
+
+(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+
+(use-package erlang
+    :ensure t
+    )
+
+(use-package lsp-mode
+    ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+    :init
+    (setq lsp-keymap-prefix "C-l")
+    (setq lsp-log-io t)
+    :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+              ;; if you want which-key integration
+              (lsp-mode . lsp-enable-which-key-integration))
+    :config
+    (add-hook 'erlang-mode-hook #'lsp)
+    :commands lsp)
+
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package company
+    :ensure t
+    :config
+    (setq company-idle-delay 0)
+    (setq company-minimum-prefix-length 3)
+    (global-company-mode t))
+(use-package company-lsp :commands company-lsp)
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
 
 
+;; Enable LSP Origami Mode (for folding ranges)
+(use-package lsp-origami
+    :ensure t
+    :init
+    (add-hook 'origami-mode-hook #'lsp-origami-mode)
+    (add-hook 'erlang-mode-hook #'origami-mode))
 
+;; Web-mode: Initialize web-mode and recognize extensions. Also
+;; consider the possibility of JSX files with a .js extension istead
+;; of .jsx.
+(use-package web-mode
+    :ensure t
+    :init
+    (setq web-mode-content-types-alist '(("jsx" . "\\.tsx\\'")))
+    (setq web-mode-content-types-alist '(("jsx" . "\\.js\\'")))
+    :config
+    (add-to-list 'auto-mode-alist '("\\.erb?\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.js[x]?\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.ts[x]?\\'" . web-mode)))
 
+;;Add node_modules path
+(use-package add-node-modules-path
+    :ensure t)
+
+;; Run prettier on save if web-mode
+(eval-after-load 'web-mode
+    '(progn
+         (add-hook 'web-mode-hook #'add-node-modules-path)
+         (add-hook 'web-mode-hook #'prettier-js-mode)))
+
+;; GraphQL
+(use-package graphql-mode
+    :ensure t)
+
+;; Yaml-mode
+(use-package yaml-mode :ensure t)
+
+;; Rust
+(use-package rust-mode
+    :ensure t)
+
+;; Markdown
+(use-package markdown-mode
+    :ensure t
+    :mode (("README\\.md\\'" . gfm-mode)
+              ("\\.md\\'" . markdown-mode)
+              ("\\.markdown\\'" . markdown-mode))
+    :init (setq markdown-command "multimarkdown"))
+
+;; Flycheck
+;; Flycheck is used for on-the-fly linting. We set the
+;; indication mode to nil, because otherwise it conflicts with the
+;; line numbers. This disables the indicators in the fringe, but
+;; still shows the marked errors in the buffer. We set a zero delay
+;; to show the error message on the status bar below, and set a 0.2
+;; second delay to avoid machine-gunning eslint.
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("28c928e0dca7617ba666c429183ceb11c582d2d2977a646ac7a0985513799e53" "1c082c9b84449e54af757bcae23617d11f563fc9f33a832a8a2813c4d7dfb652" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" "10461a3c8ca61c52dfbbdedd974319b7f7fd720b091996481c8fb1dded6c6116" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "100e7c5956d7bb3fd0eebff57fde6de8f3b9fafa056a2519f169f85199cc1c96" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "c9ddf33b383e74dac7690255dd2c3dfa1961a8e8a1d20e401c6572febef61045" "36ca8f60565af20ef4f30783aa16a26d96c02df7b4e54e9900a5138fb33808da" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))
- '(package-selected-packages
-   '(edts dockerfile-mode smart-mode-line counsel ace-window editorconfig erlang try which-key use-package)))
+    '(custom-safe-themes
+         (quote
+             ("28c928e0dca7617ba666c429183ceb11c582d2d2977a646ac7a0985513799e53" "1c082c9b84449e54af757bcae23617d11f563fc9f33a832a8a2813c4d7dfb652" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" "10461a3c8ca61c52dfbbdedd974319b7f7fd720b091996481c8fb1dded6c6116" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "100e7c5956d7bb3fd0eebff57fde6de8f3b9fafa056a2519f169f85199cc1c96" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "c9ddf33b383e74dac7690255dd2c3dfa1961a8e8a1d20e401c6572febef61045" "36ca8f60565af20ef4f30783aa16a26d96c02df7b4e54e9900a5138fb33808da" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+    '(package-selected-packages
+         (quote
+             (company lsp-ui edts dockerfile-mode smart-mode-line counsel ace-window editorconfig erlang try which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
