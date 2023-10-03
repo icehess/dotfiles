21de94586f3e14f5804b75a17a8a52fea0be12ec
Hesaam Farhang <icehess@gmail.com>
2023-10-03 11:53:44 -0700

    wezterm: disable click on links

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -39,5 +39,26 @@ config.keys = {
   },
 }
 
+config.mouse_bindings = {
+    -- Disable the default click behavior
+    {
+      event = { Up = { streak = 1, button = "Left"} },
+      mods = "NONE",
+      action = wezterm.action.DisableDefaultAssignment,
+    },
+    -- Ctrl-click will open the link under the mouse cursor
+    {
+        event = { Up = { streak = 1, button = "Left" } },
+        mods = "CTRL",
+        action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+    -- Disable the Ctrl-click down event to stop programs from seeing it when a URL is clicked
+    {
+        event = { Down = { streak = 1, button = "Left" } },
+        mods = "CTRL",
+        action = wezterm.action.Nop,
+    },
+}
+
 -- and finally, return the configuration to wezterm
 return config
