ad281442f8a8c5d7e15583b15963d1ad223bff97
Hesaam Farhang <icehess@gmail.com>
2025-09-24 22:04:21 -0700

    linuxbrew: fix manpath

--- .dotfiles-site/linuxbrew.sh
+++ .dotfiles-site/linuxbrew.sh
@@ -13,28 +13,16 @@ if [ -f /home/linuxbrew/.linuxbrew/bin/brew ]; then
     # If not running interactively, don't do anything
     [ -z "$PS1" ] && return
 
-    export MANPATH="${MANPATH+$MANPATH:}/home/linuxbrew/.linuxbrew/share/man";
-    if [ -n "${INFOPATH}" ]; then
-        export INFOPATH="${INFOPATH}:/home/linuxbrew/.linuxbrew/share/info";
-    else
-        export INFOPATH="/home/linuxbrew/.linuxbrew/share/info";
-    fi
+    # export MANPATH="${MANPATH+$MANPATH:}/home/linuxbrew/.linuxbrew/share/man";
+    # export INFOPATH="${INFOPATH+$INFOPATH:}/home/linuxbrew/.linuxbrew/share/info";
 
     if [ -d /home/linuxbrew/bash-completion ]; then
-        if [ -n "$BASH_COMPLETION_USER_DIR" ]; then
-            export BASH_COMPLETION_USER_DIR="$BASH_COMPLETION_USER_DIR:/home/linuxbrew/bash-completion"
-        else
-            export BASH_COMPLETION_USER_DIR="/home/linuxbrew/bash-completion"
-        fi
+        export BASH_COMPLETION_USER_DIR="${BASH_COMPLETION_USER_DIR+$BASH_COMPLETION_USER_DIR:}/home/linuxbrew/bash-completion"
     elif [ -d /home/linuxbrew/.linuxbrew/etc/bash_completion.d ]; then
         . /home/linuxbrew/.linuxbrew/etc/bash_completion.d/brew
     fi
 
     if [ -d /home/linuxbrew/.linuxbrew/share/applications ]; then
-      if [ -n "${XDG_DATA_DIRS}" ]; then
-        export XDG_DATA_DIRS="${XDG_DATA_DIRS}:/home/linuxbrew/.linuxbrew/share"
-      else
-        export XDG_DATA_DIRS="/home/linuxbrew/.linuxbrew/share"
-      fi
+        export XDG_DATA_DIRS="${XDG_DATA_DIRS+$XDG_DATA_DIRS:}/home/linuxbrew/.linuxbrew/share"
     fi
 fi
